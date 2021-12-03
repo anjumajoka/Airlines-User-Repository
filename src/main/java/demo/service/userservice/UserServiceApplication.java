df3caad62e0801505c2119bb971606c2446ac883
anjumajoka <62169121+anjumajoka@users.noreply.github.com>
2021-12-03 14:17:59 -0600

    swagger config

--- src/main/java/demo/service/userservice/UserServiceApplication.java
+++ src/main/java/demo/service/userservice/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServicceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServicceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
